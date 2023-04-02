6f8768c090ffff03219ea0980b290592d529319e
Logan Hickman <JarJarBinks2001@gmail.com>
2023-04-02 19:06:09 -0400

    Save delegates

--- Assets/Scripts/Menu/SettingsMenuHandler.cs
+++ Assets/Scripts/Menu/SettingsMenuHandler.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using OpenCover.Framework.Model;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -8,27 +9,27 @@ namespace Menu
 {
     public class SettingsMenuHandler : MonoBehaviour
     {
-        private RectTransform[] childrenPanels;
+        private SettingsMenuHandler[] childrenPanels;
 
         private void Start()
         {
-            childrenPanels = GetComponentsInChildren<RectTransform>(true).Where(x => x.CompareTag("Panel")).ToArray();
-            foreach (Selectable s in GetComponentsInChildren<Selectable>(true))
-            {
-                
-            }
+            childrenPanels = GetComponentsInChildren<SettingsMenuHandler>(true).Where(x => x != this).ToArray();
+            foreach (Toggle t in GetComponentsInChildren<Toggle>(true)) t.onValueChanged.AddListener(_ => SaveSettings());
+            foreach (Slider s in GetComponentsInChildren<Slider>(true)) s.onValueChanged.AddListener(_ => Invoke(nameof(SaveSettings), 0.1f));
+            foreach (Button b in GetComponentsInChildren<Button>(true)) b.onClick.AddListener(() => Invoke(nameof(SaveSettings), 0.1f));
+            foreach (Dropdown d in GetComponentsInChildren<Dropdown>(true)) d.onValueChanged.AddListener(_ => Invoke(nameof(SaveSettings), 0.1f));
+            foreach (InputField i in GetComponentsInChildren<InputField>(true)) i.onValueChanged.AddListener(_ => Invoke(nameof(SaveSettings), 0.1f));
             SelectPanel(childrenPanels[0].gameObject);
         }
 
         public void SelectPanel(GameObject panel)
         {
-            foreach (RectTransform go in childrenPanels) go.gameObject.SetActive(go.gameObject == panel);
+            foreach (SettingsMenuHandler p in childrenPanels) p.gameObject.SetActive(p.gameObject == panel);
         }
 
-        private UnityAction SaveSettings()
+        private void SaveSettings()
         {
-            return null;
+            Settings.SaveSettings();
         }
-
     }
 }
