33abc0e22916a3e5c38822d5859dce3ba54810eb
Logan Hickman <JarJarBinks2001@gmail.com>
2023-04-10 15:15:00 -0400

    Added Weighted Object Randomizer

---
+++ Assets/Scripts/WeightedObjectRandomizer.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/// <summary>
+/// Handles randomization of
+/// </summary>
+[Serializable]
+public class WeightedObjectRandomizer : MonoBehaviour
+{
+    /// <summary>
+    /// All weighted objects to spawn
+    /// </summary>
+    [SerializeField]
+    private WeightedObject[] weightedObjects;
+
+    /// <summary>
+    /// Selects an object randomly based on weights
+    /// </summary>
+    /// <typeparam name="T">Expected type of object to return</typeparam>
+    /// <returns>Randomly selected object</returns>
+    public T GetRandomObject<T>() where T : Object
+    {
+        return GetRandomObject().GetComponent<T>();
+    }
+
+    /// <summary>
+    /// Selects an object randomly based on weights
+    /// </summary>
+    /// <returns>Randomly selected object</returns>
+    public GameObject GetRandomObject()
+    {
+        int totalWeight = weightedObjects.Sum(x => x.weight);
+        int i = Random.Range(1, totalWeight+1);
+
+        foreach (WeightedObject wo in weightedObjects)
+        {
+            if (i > wo.weight) i -= wo.weight;
+            else return wo.weightedObject;
+        }
+
+        Debug.LogError("Could not find random object for some reason");
+        return null;
+    }
+}
+
+/// <summary>
+/// Container for information of weighted object
+/// </summary>
+[Serializable]
+public struct WeightedObject
+{
+    /// <summary>
+    /// Object to select
+    /// </summary>
+    public GameObject weightedObject;
+    /// <summary>
+    /// Weight of object
+    /// </summary>
+    public int weight;
+
+    public WeightedObject(GameObject weightedObject, int weight)
+    {
+        this.weightedObject = weightedObject;
+        this.weight = weight;
+    }
+}
